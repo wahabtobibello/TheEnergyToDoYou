e42200d893e5db654c9405f11b44c5dfedad8bc7
tobibello <bello.tobi@outlook.com>
2017-06-05 15:41:47 +0100

    added izimodal plugin

--- js/plugin.js
+++ js/plugin.js
@@ -1,14 +1,7 @@
 /**
  * Created by BelloPC on 17/05/2017.
  */
-const image = document.querySelector('.header');
-const btnSave = document.querySelector('#save');
-
-$('.header form').submit(e => {
-    console.log('ygygg');
-    domtoimage.toJpeg(image, {quality: 0.95})
-        .then((dataUrl) => {
-            btnSave.download = 'my-image-name.jpeg';
-            btnSave.href = dataUrl;
-        });
+$("#share-modal").iziModal({
+    autoOpen: false,
+    closeButton: true
 });
