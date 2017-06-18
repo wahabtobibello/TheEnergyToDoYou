50a50b1d7da9ccaefcd9d828d361f41cc068667c
tobibello <bello.tobi@outlook.com>
2017-06-18 12:06:29 +0100

    major code refactor

--- js/index.js
+++ js/index.js
@@ -39,185 +39,205 @@
             }
         };
     };
+    const addClipArt = (parent, clipArtObj, iText) => {
+        clipArtObj.scaleToHeight(parent.height);
+        clipArtObj.scaleToWidth(parent.width);
+        iText.scaleToHeight(parent.height / 4);
+        parent.add(clipArtObj);
+        parent.add(iText);
+    };
+    const getObjectWithType = (canvas, type) => {
+        for (let i = 0; i < canvas.size(); i++) {
+            let item = canvas.item(i);
+            if (item.type === type) {
+                return item;
+            }
+        }
+        return null;
+    }
+    let canvas = new fabric.Canvas('image-canvas', {
+        width: 846,
+        height: 846
+    });
+    let renderClipArtAndTextbox = new Promise((resolve, reject) => {
+        fabric.loadSVGFromURL('./img/the-energy-to.svg', function (objects, options) {
+            let clipArtObj = fabric.util.groupSVGElements(objects, options);
+            clipArtObj.selectable = false;
+            clipArtObj.evented = false;
+            clipArtObj.hasControls = false;
+            clipArtObj.hasRotatingPoint = false;
+            clipArtObj.hasBorders = false;
+            clipArtObj.setOriginX("center");
+            clipArtObj.setOriginY("center");
+            resolve(clipArtObj);
+        });
+    });
     const startUp = (canvas) => {
-        let textbox = new fabric.Textbox("", {
+        renderClipArtAndTextbox.then((clipArtObj) => {
+            let textbox = new fabric.Textbox("", {
+                fontFamily: "AvenyT-Black",
+                textAlign: "center",
+                fill: "#d80b2c",
+                originX: "center",
+                originY: "center",
+                cursorColor: "#d80b2c",
+                cursorWidth: 5,
+                evented: false,
+                hasControls: false,
+                hasRotatingPoint: false,
+                hasBorders: false,
+                skewY: -10,
+                top: 735,
+                left: 432,
+            });
+            clipArtObj.setTop(423);
+            clipArtObj.setLeft(423);
+            addClipArt(canvas, clipArtObj, textbox);
+            initializeTextbox(textbox);
+        });
+    };
+    startUp(canvas);
+    canvas.on('mouse:down', (e) => {
+        for (let i = 0; i < canvas.size(); i++) {
+            let item = canvas.item(i);
+            if (item.type !== "image") {
+                canvas.setActiveObject(item);
+                break;
+            }
+        }
+        canvas.renderAll();
+    });
+    canvas.on('mouse:up', (e) => {
+        let activeObject = e.target;
+        if (activeObject && activeObject.type === "image") {
+            let tlX = activeObject.aCoords.tl.x;
+            let tlY = activeObject.aCoords.tl.y;
+            let brX = activeObject.aCoords.br.x;
+            let brY = activeObject.aCoords.br.y;
+            let canvasWidth = canvas.width;
+            let canvasHeight = canvas.height;
+            let currentWidth = activeObject.width * activeObject.scaleX;
+            let currentHeight = activeObject.height * activeObject.scaleY;
+            if (tlX > 0) {
+                activeObject.setLeft(currentWidth / 2);
+            }
+            if (tlY > 0) {
+                activeObject.setTop(currentHeight / 2);
+            }
+            if (brX < canvasWidth) {
+                activeObject.setLeft(canvasWidth - (currentWidth / 2));
+            }
+            if (brY < canvasHeight) {
+                activeObject.setTop(canvasHeight - (currentHeight / 2));
+            }
+            activeObject.setCoords();
+        } else {
+            initializeTextbox(getObjectWithType(canvas, "textbox"));
+        }
+        canvas.renderAll();
+    });
+    const renderImage = (canvas, img) => {
+        let clipArtObj = getObjectWithType(canvas, "path-group");
+        let inputText = getObjectWithType(canvas, "textbox").getText();
+        let textObj = new fabric.Text(inputText, {
+            left: 9,
+            top: 312,
             fontFamily: "AvenyT-Black",
             textAlign: "center",
             fill: "#d80b2c",
             originX: "center",
             originY: "center",
-            cursorColor: "#d80b2c",
-            cursorWidth: 5,
             evented: false,
             hasControls: false,
             hasRotatingPoint: false,
             hasBorders: false,
             skewY: -10,
-            top: canvas.height * 2 / 2.63
         });
-        let createSvg = new Promise((resolve, reject) => {
-            fabric.loadSVGFromURL('img/the-energy-to.svg', function (objects, options) {
-                let energyTo = fabric.util.groupSVGElements(objects, options);
-                energyTo.selectable = false;
-                energyTo.evented = false;
-                energyTo.top = canvas.height / 2;
-                energyTo.left = canvas.width / 2;
-                energyTo.originX = "center";
-                energyTo.originY = "center";
-                energyTo.hasControls = false;
-                energyTo.hasRotatingPoint = false;
-                energyTo.hasBorders = false;
-                resolve(energyTo);
-            });
+        let imageObj = new fabric.Image(img, {
+            hasRotatingPoint: false,
+            hasControls: false,
+            hasBorders: false,
+            originX: "center",
+            originY: "center",
+            left: canvas.width / 2,
+            top: canvas.height / 2,
         });
-        createSvg.then((energyTo) => {
-            energyTo.scaleToHeight(canvas.height);
-            energyTo.scaleToWidth(canvas.width);
-            textbox.scaleToHeight(canvas.height / 5);
-            canvas.add(energyTo);
-            canvas.add(textbox);
-            canvas.centerObject(energyTo);
-            canvas.centerObjectH(textbox);
-            initializeTextbox(textbox);
-            canvas.renderAll();
+        let groupObj = new fabric.Group([], {
+            hasControls: false,
+            hasRotatingPoint: false,
+            hasBorders: false,
+            selectable: false,
+            evented: false,
+            width: 846,
+            height: 846,
+            originX: "center",
+            originY: "center",
+            left: canvas.width / 2,
+            top: canvas.height / 2,
         });
-        let createImage = new Promise((resolve, reject) => {
-            $('#fileInput').change(function () {
-                if (!this.files[0]) {
-                    reject(new Error("No image selected"));
-                    return;
-                }
-                let u = URL.createObjectURL(this.files[0]);
-                let img = new Image;
-                img.src = u;
-                img.onload = function () {
-                    let backImage = new fabric.Image(img, {
-                        hasRotatingPoint: false,
-                        hasControls: false,
-                        hasBorders: false,
-                        originX: "center",
-                        originY: "center",
-                        left: canvas.width / 2,
-                        top: canvas.height / 2
-                    });
-                    let imageObj = canvas.item(0);
-                    if (imageObj && imageObj.type === "image")
-                        canvas.remove(imageObj);
-                    canvas.add(backImage)
-                        .sendToBack(backImage);
-                    if (backImage.width <= backImage.height) {
-                        backImage.scaleToWidth(backImage.canvas.width);
-                    }
-                    else if (backImage.width > backImage.height) {
-                        backImage.scaleToHeight(backImage.canvas.height);
-                    }
-                    canvas.renderAll();
-                    resolve(canvas);
-                };
-            });
-        });
-        createImage.then((canvas) => {
-            return new Promise((resolve) => {
-                canvas.defaultCursor = "default";
-                let energyTo = canvas.item(1);
-                let action = canvas.item(2);
-                action.exitEditing();
-                energyTo.animate("scaleX", energyTo.scaleX / 1.5, {
-                    duration: 700,
-                    onChange: canvas.renderAll.bind(canvas),
-                    ease: "easeOutSine"
-                }).animate("scaleY", energyTo.scaleY / 1.5, {
-                    duration: 700,
-                    onChange: canvas.renderAll.bind(canvas),
-                    ease: "easeOutSine"
-                });
-                action.animate("scaleX", action.scaleX / 1.5, {
-                    duration: 700,
-                    onChange: canvas.renderAll.bind(canvas),
-                    ease: "easeOutSine"
-                }).animate("top", canvas.height * 2 / 2.97, {
-                    duration: 700,
-                    onChange: canvas.renderAll.bind(canvas),
-                    ease: "easeOutSine"
-                }).animate("scaleY", action.scaleY / 1.5, {
-                    duration: 700,
-                    onChange: canvas.renderAll.bind(canvas),
-                    ease: "easeOutSine"
-                });
-                canvas.renderAll();
-                $addPhotoOnly.hide();
-                $moreOptions.show();
-                resolve(canvas);
-            })
-        }).then((canvas) => {
-            // console.log(convertCanvasToImage(canvas.getElement()).src);
-        });
-        canvas.on('mouse:down', (e) => {
-            for (let i = 0; i < canvas.size(); i++) {
-                let item = canvas.item(i);
-                if (item.type !== "image") {
-                    canvas.setActiveObject(item);
-                    break;
-                }
-            }
-            canvas.renderAll();
-        });
-        canvas.on('mouse:up', (e) => {
-            let activeObject = e.target;
-            if (activeObject && activeObject.type === "image") {
-                let tlX = activeObject.aCoords.tl.x;
-                let tlY = activeObject.aCoords.tl.y;
-                let brX = activeObject.aCoords.br.x;
-                let brY = activeObject.aCoords.br.y;
-                let canvasWidth = canvas.width;
-                let canvasHeight = canvas.height;
-                let currentWidth = activeObject.width * activeObject.scaleX;
-                let currentHeight = activeObject.height * activeObject.scaleY;
-                if (tlX > 0) {
-                    activeObject.setLeft(currentWidth / 2);
-                }
-                if (tlY > 0) {
-                    activeObject.setTop(currentHeight / 2);
-                }
-                if (brX < canvasWidth) {
-                    activeObject.setLeft(canvasWidth - (currentWidth / 2));
-                }
-                if (brY < canvasHeight) {
-                    activeObject.setTop(canvasHeight - (currentHeight / 2));
-                }
-                activeObject.setCoords();
-            } else {
-                for (let i = 0; i < canvas.size(); i++) {
-                    let item = canvas.item(i);
-                    if (item.type === "textbox") {
-                        initializeTextbox(canvas.item(i));
-                        break;
-                    }
-                }
-            }
-            canvas.renderAll();
-        });
+
+        canvas.clear();
+        canvas.defaultCursor = "default";
+
+        canvas.add(imageObj);
+        if (imageObj.width <= imageObj.height) {
+            imageObj.scaleToWidth(imageObj.canvas.width);
+        }
+        else if (imageObj.width > imageObj.height) {
+            imageObj.scaleToHeight(imageObj.canvas.height);
+        }
+
+        canvas.add(groupObj);
+        addClipArt(groupObj, clipArtObj, textObj);
+        clipArtObj.setLeft(0);
+        clipArtObj.setTop(0);
+
+        canvas.sendToBack(imageObj);
+        // groupObj.animate("scaleX", groupObj.scaleX / 1.5, {
+        //     duration: 700,
+        //     onChange: canvas.renderAll.bind(canvas),
+        //     ease: "easeOutSine"
+        // }).animate("scaleY", groupObj.scaleY / 1.5, {
+        //     duration: 700,
+        //     onChange: canvas.renderAll.bind(canvas),
+        //     ease: "easeOutSine"
+        // });
+        $addPhotoOnly.hide();
+        $moreOptions.show();
     };
-    let canvas = new fabric.Canvas('image-canvas', {
-        width: 846,
-        height: 846
+
+    $("#file-input").change(function () {
+        if (!this.files[0]) {
+            console.error("No image selected");
+            return;
+        }
+        let u = URL.createObjectURL(this.files[0]);
+        let img = new Image;
+        img.src = u;
+        img.onload = function () {
+            renderImage(canvas, img);
+        };
     });
-    startUp(canvas);
+
     $saveLinkBtn.click((e) => {
         $saveLink.attr('href', convertCanvasToImage(canvas.getElement()).src);
         $saveLink.attr('download', 'image.jpg');
         $saveLink.click();
     });
+
     $startOver.click((e) => {
         canvas.clear();
         startUp(canvas);
         $moreOptions.hide();
     });
+
     $addPhoto.click((e) => {
-        $('#fileInput').click();
+        $('#file-input').click();
     });
+
     $shareBtn.click((e) => {
         event.preventDefault();
         $('#share-modal').iziModal('open');
     });
+
 }());
