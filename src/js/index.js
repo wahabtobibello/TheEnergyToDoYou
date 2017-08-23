fa81db6cf0631388439ea20f5294864c5ebc4959
Tobi Bello <bello.tobi@outlook.com>
2017-08-23 10:06:32 +0100

    safari zoom bug fiz attempt

--- src/js/index.js
+++ src/js/index.js
@@ -220,7 +220,7 @@
           }
           if (textStr !== "") {
             this.placeholderObj._set("opacity", 0);
-          }else{
+          } else {
             this.placeholderObj._set("opacity", 0.5);
           }
           this.setText(textStr);
@@ -325,6 +325,17 @@
         });
       }
     });
+    // if (/iPhone/.test(navigator.userAgent) && !window.MSStream) {
+    //   $(document).on("focus", "input, textarea, select", function () {
+    //     $('meta[name=viewport]').remove();
+    //     $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">');
+    //   });
+
+    //   $(document).on("blur", "input, textarea, select", function () {
+    //     $('meta[name=viewport]').remove();
+    //     $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
+    //   });
+    // }
   });
   $fileInput.click(function () {
     this.value = "";
