6a7c312a7d2523a02cde5c9ce19836416a285e9a
Tobi Bello <bello.tobi@outlook.com>
2017-08-23 10:24:42 +0100

    update production files

--- dist/js/index.js
+++ dist/js/index.js
@@ -46741,6 +46741,17 @@ var share2social = function share2social($, canvas) {
         });
       }
     });
+    if (/iPhone/.test(navigator.userAgent) && !window.MSStream) {
+      $(document).on("focus", "input, textarea, select", function () {
+        $('meta[name=viewport]').remove();
+        $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">');
+      });
+
+      $(document).on("blur", "input, textarea, select", function () {
+        $('meta[name=viewport]').remove();
+        $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1">');
+      });
+    }
   });
   $fileInput.click(function () {
     this.value = "";
